3112b7c3510f0403ec45fe6bfaf3c9f2271f291d
Nqv1208 <qvinhkl10@gmail.com>
2024-08-23 19:25:26 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
 
-
-
-window.onscroll = function() {scrollFunction()};
-    // khai báo hàm scrollFunction
+document.addEventListener("DOMContentLoaded", function() {
     function scrollFunction() {
-        // Kiểm tra vị trí hiện tại của con trỏ so với nội dung trang
         if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-            //nếu lớn hơn 2000px thì hiện button
             document.getElementById("back-top").style.display = "block";
         } else {
-             //nếu nhỏ hơn 20px thì ẩn button
             document.getElementById("back-top").style.display = "none";
         }
     }
-    //gán sự kiện click cho button
-    document.getElementById('back-top').addEventListener("click", function(){
-        //Nếu button được click thì nhảy về đầu trang
+
+    document.getElementById('back-top').addEventListener("click", function() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     });
+
+    function scrollFunction2() {
+        var header = document.getElementById('header');
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            header.classList.add('no-top');
+            header.classList.remove('top');
+        } else {
+            header.classList.add('top');
+            header.classList.remove('no-top');
+        }
+    }
+
+    window.onscroll = function() {
+        scrollFunction();
+        scrollFunction2();
+    };
+
+    const elements = document.querySelectorAll('.service-single, .featured-cars-single');
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    elements.forEach(element => {
+        element.classList.add('hidden');
+        observer.observe(element);
+    });
+});
+
